34509cac1898bee17edd094a62f96e1f55b0d6bc
Lavishq <v.whitehearted@simplelogin.co>
2022-12-04 12:15:54 +0530

    input and titles added

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Healthbot</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2> Eat Smart, Live Healthy - Unlock the Benefits of the Right Foods with Our Proven System</h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={(e) =>{setUserInput(e.target.value)}} />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        
       </div>
       <div className="badge-container grow">
         <a
@@ -33,5 +44,6 @@ const Home = () => {
     </div>
   );
 };
+
 
 export default Home;
